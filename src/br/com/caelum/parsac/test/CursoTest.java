8f5810598c163e623e89c5d3e3ed14dfda92455e
Vitor Mattos <vitor.mattos@caelum.com.br>
2013-03-27 18:55:05 -0300

    Separando testes

--- src/br/com/caelum/parsac/test/CursoTest.java
+++ src/br/com/caelum/parsac/test/CursoTest.java
@@ -40,39 +40,6 @@ public class CursoTest {
 
 		Curso curso = new Curso("FJ-00", "Formação Teste", secoes);
 
-		XStream xstream = new XStream();
-		xstream.processAnnotations(Curso.class);
-		xstream.processAnnotations(Exercicio.class);
-		xstream.processAnnotations(Alternativa.class);
-		xstream.processAnnotations(Aberto.class);
-		xstream.processAnnotations(MultiplaEscolha.class);
-		
-
-		File xml = new File("teste.xml");
-		
-//		Curso cursoDes = (Curso)xstream.fromXML("<?xml version='1.0' encoding='UTF-8'?>  " +
-//				"								<curso>" +
-//				"									<sigla>FJ-00</sigla>" +
-//				"									<titulo>titulo</titulo>" +
-//				"									<secoes>" +
-//				"										<secao>" +
-//				"											<numero>1</numero>" +
-//				"											<titulo>Titulo da secao</titulo>" +
-//				"											<explicacao>Explicacao</explicacao>" +
-//				"											<exercicios>" +
-//				"												<exercicio-aberto>" +
-//				"													<enunciado>Enunciado</enunciado>" +
-//				"													<resposta>La Resposta</resposta>" +
-//				"												</exercicio-aberto>" +
-//				"												<exercicio-multiplaEscolha>" +
-//				"												</exercicio-multiplaEscolha>" +
-//				"											</exercicios>" +
-//				"										</secao>" +
-//				"									</secoes>" +
-//				"								</curso>");
-		
-		Curso cursoDes = (Curso)xstream.fromXML(xml);
-		
-		System.out.println(cursoDes);
+		System.out.println(curso);
 	}
 }
