e40c7919fdb6bfc6d49c669f80c6683d02e4ccd5
Rômulo Henrique <romulo.henriquemv@caelum.com.br>
2013-04-01 19:31:38 -0300

    Terminando CursoTest

--- src/test/java/br/com/caelum/parsac/test/curso/CursoTest.java
+++ src/test/java/br/com/caelum/parsac/test/curso/CursoTest.java
@@ -1,29 +1,25 @@
 package br.com.caelum.parsac.test.curso;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 import br.com.caelum.parsac.factory.CursoFactory;
-import br.com.caelum.parsac.modelo.Alternativa;
 import br.com.caelum.parsac.modelo.Curso;
 import br.com.caelum.parsac.modelo.Exercicio;
+import br.com.caelum.parsac.modelo.MultiplaEscolha;
 import br.com.caelum.parsac.modelo.Secao;
 
 public class CursoTest {
 
 	@Test
-	public void testaCurso() throws FileNotFoundException {
+	public void testaCurso() {
 
 		Curso curso = new CursoFactory().constroiCurso();
 
 		Secao secao = curso.getSecoes().get(0);
 		Exercicio exercicioAberto = secao.getExercicios().get(0);
-		Exercicio exercicioMultiplaEscolha = secao.getExercicios().get(1);
-		//List<Alternativa> alternativas = new ArrayList<Alternativa>();
+		MultiplaEscolha exercicioMultiplaEscolha = (MultiplaEscolha) secao
+				.getExercicios().get(1);
 
 		Assert.assertEquals("FJ-00", curso.getSigla());
 		Assert.assertEquals("Formação Teste", curso.getTitulo());
@@ -32,8 +28,14 @@ public class CursoTest {
 		Assert.assertEquals("Explicacao da sessao", secao.getExplicacao());
 		Assert.assertEquals("Enunciado exercício aberto",
 				exercicioAberto.getEnunciado());
-		Assert.assertEquals("Enunciado exercício múltipla escolha", exercicioMultiplaEscolha.getEnunciado());
-		//Assert.assertEquals("Op 1", alternativas.get(0).getTexto());
-		//Assert.assertEquals("Op 2", alternativas.get(1).getTexto());
+		Assert.assertEquals("Enunciado exercício múltipla escolha",
+				exercicioMultiplaEscolha.getEnunciado());
+		Assert.assertEquals("Op 1", exercicioMultiplaEscolha.getAlternativas()
+				.get(0).getTexto());
+		Assert.assertEquals("Op 2", exercicioMultiplaEscolha.getAlternativas()
+				.get(1).getTexto());
+		Assert.assertEquals("Op 1", exercicioMultiplaEscolha.getResposta());
+
 	}
+
 }
