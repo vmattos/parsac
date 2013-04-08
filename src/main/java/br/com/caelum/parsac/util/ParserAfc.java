c3686a3f0d15da12361e6b1a4b7a4aa6a595b6ef
Vitor Mattos <vitor.mattos@caelum.com.br>
2013-04-08 16:30:33 -0300

    Criando parser para section e metodo parseiaCurso

--- src/main/java/br/com/caelum/parsac/util/ParserAfc.java
+++ src/main/java/br/com/caelum/parsac/util/ParserAfc.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import br.com.caelum.parsac.modelo.Curso;
+import br.com.caelum.parsac.modelo.Secao;
+
 public class ParserAfc {
 
 	private List<String> linguagens = new ArrayList<String>();
@@ -20,7 +23,7 @@ public class ParserAfc {
 		linguagens.add("c#");
 	}
 	
-	public String parseia(String string){
+	public String parseiaTagsOnline(String string){
 
 		for (String linguagem : linguagens) {
 			string = string.replaceAll(Pattern.quote("[") + "(" + linguagem + ")" + Pattern.quote("]"), "[code " + linguagem + "]");
@@ -36,6 +39,20 @@ public class ParserAfc {
 		string = string.replaceAll("<code>", "%%");
 		string = string.replaceAll("</code>", "%%");
 		
+		string = string.replaceAll("<h1>", "[section ");
+		string = string.replaceAll("</h1>", "]");
+		
+		
 		return string;
 	}
+	
+	public String parseiaCurso(Curso curso, int numeroDaSecao) {
+		Secao secao = curso.getSecoes().get(numeroDaSecao);
+		String texto = "[chapter " + secao.getTitulo() + "]";
+		
+		texto += "\n" + parseiaTagsOnline(secao.getExplicacao()); 
+		
+		return texto;
+	}
+	
 }
