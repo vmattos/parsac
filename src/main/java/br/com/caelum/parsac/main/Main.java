2b7506cefa19edea75681f66e4aad27e5f1c2082
Vitor Mattos <vitor.mattos@caelum.com.br>
2013-04-09 14:57:35 -0300

    Classe Main

---
+++ src/main/java/br/com/caelum/parsac/main/Main.java
@@ -0,0 +1,45 @@
+package br.com.caelum.parsac.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import br.com.caelum.parsac.builder.XStreamBuilder;
+import br.com.caelum.parsac.modelo.Curso;
+import br.com.caelum.parsac.modelo.Secao;
+import br.com.caelum.parsac.util.ParserAfc;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		XStream xstream = new XStreamBuilder().processaAnotacoesXStream();
+		File xml = new File("teste.xml");
+		ParserAfc parser = new ParserAfc();
+		Curso arquivoDeserializado = (Curso) xstream.fromXML(xml);
+		
+		for (Secao secao : arquivoDeserializado.getSecoes()) {
+			int numeroDaSecao = secao.getNumero();
+
+			String nomeAfc = secao.getTitulo().replaceAll(" ", "-")
+					.toLowerCase();
+
+			File arquivoAfc = new File(numeroDaSecao + "-" + nomeAfc + ".afc");
+
+			BufferedWriter br = new BufferedWriter(new FileWriter(arquivoAfc));
+
+			String cursoParseado = parser.parseiaCurso(arquivoDeserializado,
+					numeroDaSecao - 1);
+
+			System.out.println(cursoParseado);
+
+			br.write(cursoParseado);
+			br.close();
+		}
+
+	}
+}
