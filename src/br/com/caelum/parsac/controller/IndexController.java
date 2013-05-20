2aa471124b4395976c68ee8e420b56d4e43a317b
Vitor Mattos <vitor.mattos@caelum.com.br>
2013-05-20 15:44:00 -0300

    Criando controller

---
+++ src/br/com/caelum/parsac/controller/IndexController.java
@@ -0,0 +1,29 @@
+package br.com.caelum.parsac.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+import com.thoughtworks.xstream.XStream;
+
+@Resource
+public class IndexController {
+
+	private final Result result;
+
+	public IndexController(Result result) {
+		this.result = result;
+	}
+
+	@Path("/")
+	public void index() throws IOException {
+		result.include("variable", "VRaptor!");
+	}
+
+}
